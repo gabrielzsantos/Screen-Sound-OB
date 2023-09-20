6080ec7c9f12650e4b0c881bd4df5bea47b78f36
Gabriel Santos <cl.gabrielzsantos@gmail.com>
2023-09-20 15:03:44 -0300

    iniciando screen-sound-ob

---
+++ ScreenSound/Musica.cs
@@ -0,0 +1,21 @@
+﻿class Musica
+{
+    public string nome;
+    public string artista;
+    public int duracaoEmSegundos;
+    public bool disponivel;
+
+    public void ExibirfixaTecnica()
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duração: {duracaoEmSegundos}");
+        if (disponivel)
+        {
+            Console.WriteLine("Disponivel no plano\n");
+        } else
+        {
+            Console.WriteLine("Adquira no plano Plus+\n");
+        }
+    }
+}
---
+++ ScreenSound/Program.cs
@@ -0,0 +1,15 @@
+﻿Musica musica1 = new Musica();
+musica1.nome = "Moça";
+musica1.artista = "Tiee";
+musica1.duracaoEmSegundos = 212;
+musica1.disponivel = true;
+
+
+Musica musica2 = new Musica();
+musica2.nome = "Meu lugar";
+musica2.artista = "Arlindo Cruz";
+musica2.duracaoEmSegundos = 293;
+musica2.disponivel = false;
+
+musica1.ExibirfixaTecnica();
+musica2.ExibirfixaTecnica();
